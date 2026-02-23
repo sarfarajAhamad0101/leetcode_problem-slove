99867029b795c747991eae21700d93f197f50939
sarfaraz <sarfaraz732002@gmail.com>
2026-02-23 16:55:21 +0530

    changes done

---
+++ calculater.js
@@ -0,0 +1,12 @@
+let a = 10;
+let b = 5;
+let op = "+";
+
+let result;
+
+if (op === "+") result = a + b;
+else if (op === "-") result = a - b;
+else if (op === "*") result = a * b;
+else if (op === "/") result = a / b;
+
+console.log(result);
